41755014b904e4d4f6440e868fa9357880bee761
choisangwon <rngus3698@naver.com>
2024-02-27 17:30:35 +0900

    프로젝트 구조 설계

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CoreModule } from "../core/core.module";
+import { FeatureModule } from "../feature/feature.module";
+import { SharedModule } from "../shared/shared.module";
 
 @Module({
-  imports: [],
+  imports: [CoreModule, FeatureModule, SharedModule],
   controllers: [AppController],
   providers: [AppService],
 })
