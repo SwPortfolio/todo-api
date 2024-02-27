fd592943204f40d3b7a61090e55521dd0fa18d3b
choisangwon <rngus3698@naver.com>
2024-02-27 17:31:19 +0900

    프로젝트 구조 설계

---
+++ src/feature/feature.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+
+/**
+ * Feature Modules: 애플리케이션의 각 기능별로 모듈을 만듭니다.
+ * 각 Feature 모듈은 해당 기능과 관련된 서비스, 컨트롤러, 프로바이더 등을 포함합니다.
+ * 'UserModule', 'PostModule', CommentModule' 등의 모듈이 여기에 해당합니다.
+ */
+@Module({})
+export class FeatureModule {}
